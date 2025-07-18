b255c5ed592a88d2c6791a986e3b41b2e998585f
EG WEBCODE <kaio.ff.5340@gmail.com>
2025-07-18 02:44:00 -0300

    Atualizar o login.php

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-pkg udpdate
-pkg upgrade -y
-pkg install python -y
-pip install requests
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $user = $_POST['username'] ?? '';
+    $pass = $_POST['password'] ?? '';
+
+    if ($user === 'admin' && $pass === 'notfound404') {
+        echo "Login OK";
+    } else {
+        echo "Login FAIL";
+    }
+}
+?>
